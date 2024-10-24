a14384938cffe79d8e7c36518f80f169b620445d
surajgoraicse <surajgoraicse@gmail.com>
2024-10-24 20:29:06 +0530

    model schema

---
+++ 3dataModelling/models/Hospital-management/medicalRecords.model.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+const medicalRecordsSchema = new mongoose.Schema({
+
+}, { timestamps: true })
+
+export const MedicalRecord = mongoose.model("MedicalRecord", medicalRecordsSchema)
