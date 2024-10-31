ad90813b5d6d36536efdc580cda8cb24058ea96d
surajgoraicse <surajgoraicse@gmail.com>
2024-10-31 20:23:58 +0530

    user model

--- 4megaProject/src/models/user.model.js
+++ 4megaProject/src/models/user.model.js
@@ -56,15 +56,43 @@ const userSchema = new Schema({
 userSchema.pre("save", async function () {   // can't use arrow fn as they does not have access to this keyword
     if (!this.isModified("password")) return next();
 
-    this.password = await bcrypt.hash(this.password , 10)   // check this part (await)  TODO:
+    this.password = await bcrypt.hash(this.password, 10)   // check this part (await)  TODO:
     next();
 })
 
 
 // adding a custom methods 
 userSchema.methods.isPasswordCorrect = async function (password) {
-   return await bcrypt.compare(password , this.password)
+    return await bcrypt.compare(password, this.password)
 }
+
+userSchema.methods.generateAccessToken = function () {  // don't use callback functions
+   return  jwt.sign(
+        {
+        _id: this._id,
+        username: this.username,
+        fullName: this.fullName,
+        email: this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET ,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_SECRET,
+            
+        }
+    )
+};
+userSchema.methods.generateRefreshToken = function () { 
+    return  jwt.sign(
+        {
+        _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET ,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+            
+        }
+    )
+};
 
 
 
